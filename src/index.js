a3b1546c2593c59aded33ef839b884c660c027a3
jiwon <jiwon.=hur6@gmail.com>
2024-05-25 00:14:44 -0700

    added dialog and created delete button on projects

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './css/style.css';
-import { addProject } from './modules/project';
+import { addProject, deleteProject } from './modules/project';
 
 const addProjectButton = document.getElementById("projectButton");
+const deleteButton = document.getElementsByClassName("deleteButton");
 
 addProjectButton.addEventListener("click", function() {
   addProject();
--- src/modules/project.js
+++ src/modules/project.js
@@ -1,12 +1,28 @@
 export function addProject() {
-  const projectForm = document.getElementById("projectForm");
-
-  projectForm.style.visibility = "visible";
+  const dialog = document.getElementById("dialog");
+  dialog.showModal();
 }
 
 const projects = document.getElementById("projects");
 const projectForm = document.getElementById("projectForm");
+const dialog = document.getElementById("dialog");
+
 const submitProjectButton = document.getElementById("submitProjectButton");
+const deleteFormButton = document.getElementById("deleteFormButton");
+
+function deleteProject() {
+  document.querySelectorAll(".deleteProjectButtons").forEach(button => {
+    button.addEventListener("click", function() {
+      this.parentElement.remove();
+    });
+  });
+}
+
+deleteFormButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  document.getElementById("projectTitleInput").value = "";
+  dialog.close();
+});
 
 submitProjectButton.addEventListener("click", function(e) {
   e.preventDefault();
@@ -17,8 +33,19 @@ submitProjectButton.addEventListener("click", function(e) {
   newProjectTitle.setAttribute('type', 'button');
   newProjectTitle.classList.add("projectTitles");
   newProjectTitle.textContent = projectTitleInput;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute('type', 'button');
+  deleteButton.classList.add("deleteProjectButtons");
+  deleteButton.textContent = 'x';
+  newProjectTitle.appendChild(deleteButton);
+
   projects.appendChild(newProjectTitle);
 
   document.getElementById("projectTitleInput").value = "";
   projectForm.style.visibility = "hidden";
+
+  dialog.close();
+  deleteProject();
 });
+
